6604843e95a720899399425a4d226025f76a5b44
pariberi <parisa.berangi@gmail.com>
2023-08-15 15:32:42 +0330

    [Feature] request and load image (#3)

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import warnings
+from time import time
+
+warnings.filterwarnings('ignore')
+
+if __name__ == "__main__":
+    print('#########################################')
+    print('#      Image Clustering with MNIST      #')
+    print('#########################################')
+
+    global LOAD_START_TIME
+    LOAD_START_TIME = time()
+
+    message = 'please enter the path of an 28*28 image to predict its content : '
+    image_path = input(message)
